e74878b8491fd464c47fb7b2b73e9725126f9dd6
Babatunde Sanusi <swisskid95@gmail.com>
2025-10-06 05:02:41 +0100

    fix some more design issues

--- client/app/components/Story.tsx
+++ client/app/components/Story.tsx
@@ -13,31 +13,31 @@ export default function StorySection() {
         <h2 className="section-title">STORY</h2>
         <div className="story-text">
           <p>
-            I first saw her briefly at a friend’s Christmas party in 2015. As
-            God would have it, she came to celebrate my birthday the following
-            year, but we still didn’t get the chance to talk. Then, on Christmas
-            Day, I stylishly asked for her contact-and that simple moment marked
+            I first saw her briefly at a mutual friend’s Christmas party in
+            2015. As God would have it, she came to celebrate my birthday the
+            following year, but we still didn’t get the chance to talk. Then, on
+            Christmas Day, I asked for her contact and that simple moment marked
             the beginning of our friendship.
           </p>
           <p>
             From the start, I wanted something more than friendship, but she
-            always said she was “enjoying her singleness” and insisted we should
-            just be friends. Eventually, I stopped bringing it up (even though
-            she was always on my mind). I still remember jokingly asking her to
-            be my bestie so she could give me advice on how to talk to other
-            girls-but she wasn’t having it!
+            always said she was “enjoying her singleness” and was sturborn that
+            we should just be friends. Eventually, I stopped bringing it up
+            (even though she was always on my mind). I still remember jokingly
+            asking her to be my bestie so she could be my wing woman on how to
+            get to other girls but she wasn’t having it!
           </p>
           <p>
-            Fast forward to 2021-after I was shot, life took a turn that
+            Fast forward to 2021 after I was shot, life took a turn that
             reminded us both how precious every moment is. Three months after
-            that near-death experience, she agreed to be mine, and our love
-            story truly began.
+            that near death experience, she agreed to be mine, and our love
+            story began.
           </p>
           <p>
-            Four beautiful years later, we’re still here-loving, growing, and
+            Four beautiful years later, we’re still here loving, growing, and
             laughing together. We got engaged at our favourite restaurant,
             surrounded by memories of how far we’ve come. And now, we’re ready
-            to walk into forever-hand in hand, hearts full of God's love, joy,
+            to walk into forever hand in hand, hearts full of God's love, joy,
             peace, and harmony.
           </p>
         </div>
